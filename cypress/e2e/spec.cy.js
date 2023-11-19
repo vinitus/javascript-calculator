dce96c172d4159e0db3e7d3a7615253f29bc02fd
vinitus <tlsdnrng@gmail.com>
2023-11-19 14:20:20 +0900

    refactor: dispaly 값 체크 함수 호출 리팩토링

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,6 +4,10 @@ const clickDigitButtons = (digits = []) => {
   });
 };
 
+const checkDisplayValue = (value) => {
+  cy.get('#total').should('have.text', value);
+};
+
 describe('계산기 앱 테스트', () => {
   beforeEach('페이지 방문', () => {
     cy.visit('../../index.html');
@@ -11,7 +15,7 @@ describe('계산기 앱 테스트', () => {
 
   it('디스플레이에 기본적으로 숫자 0이 표시된다.', () => {
     // querySelector 역할
-    cy.get('#total').should('have.text', 0);
+    checkDisplayValue(0);
   });
 
   // 계산기에서 요구되는 것은 결국 결과값이 표시되어야한다.
@@ -20,18 +24,18 @@ describe('계산기 앱 테스트', () => {
     // contains()는 해당 문자열이 포함된 여러 HTML 요소 중 하나를 선택함
     // click은 해당 HTML 태그를 클릭처리
     clickDigitButtons(['1']);
-    cy.get('#total').should('have.text', 1);
+    checkDisplayValue(1);
   });
 
   // 1개의 클릭보단 큰 단위인 2개의 클릭
   it('2개의 숫자 버튼을 클릭하면 display에 숫자가 표시된다', () => {
     clickDigitButtons(['1', '2']);
-    cy.get('#total').should('have.text', 12);
+    checkDisplayValue(12);
   });
 
   // unknown의 영역 -> 3개까지 잘 작동될까?
   it('2개의 숫자 버튼을 클릭하면 display에 숫자가 표시된다', () => {
     clickDigitButtons(['1', '2', '4']);
-    cy.get('#total').should('have.text', 124);
+    checkDisplayValue(124);
   });
 });
