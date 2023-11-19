e919a9b8f496ec3718c7afb898ee5f4fc475ae67
vinitus <tlsdnrng@gmail.com>
2023-11-19 15:16:05 +0900

    TDD: 연산자 버튼 클릭에 대한 구현

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,16 @@
 function App() {
+  handlingDigitButtonClick();
+
+  document.querySelector('.operations').addEventListener('click', (e) => {
+    if (!e.target.classList.contains('operation')) return;
+
+    document.querySelector('#total').innerText = document.querySelector('#total').innerText + e.target.innerText;
+  });
+}
+
+App();
+
+function handlingDigitButtonClick() {
   const $digits = document.querySelector('.digits');
   const $total = document.querySelector('#total');
 
@@ -13,5 +25,3 @@ function App() {
 
   $digits.addEventListener('click', updateTotalDisplay);
 }
-
-App();
