97f05eea48286b6fefba9e34e0dc5a81d3e6cbf1
vinitus <tlsdnrng@gmail.com>
2023-11-19 14:16:05 +0900

    3개의 버튼 클릭에 대한 테스트 코드

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -17,11 +17,18 @@ describe('계산기 앱 테스트', () => {
     cy.get('#total').should('have.text', 1);
   });
 
+  // 1개의 클릭보단 큰 단위인 2개의 클릭
   it('2개의 숫자 버튼을 클릭하면 display에 숫자가 표시된다', () => {
-    // contains()는 해당 문자열이 포함된 여러 HTML 요소 중 하나를 선택함
-    // click은 해당 HTML 태그를 클릭처리
     cy.get('.digit').contains('1').click();
     cy.get('.digit').contains('2').click();
     cy.get('#total').should('have.text', 12);
+  });
+
+  // unknown의 영역 -> 3개까지 잘 작동될까?
+  it('2개의 숫자 버튼을 클릭하면 display에 숫자가 표시된다', () => {
+    cy.get('.digit').contains('1').click();
+    cy.get('.digit').contains('2').click();
+    cy.get('.digit').contains('4').click();
+    cy.get('#total').should('have.text', 124);
   });
 });
