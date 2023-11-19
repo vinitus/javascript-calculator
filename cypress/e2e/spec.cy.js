e4cc19f8c767588d48308276e84a5a7b828f03d1
vinitus <tlsdnrng@gmail.com>
2023-11-19 14:19:34 +0900

    refactor: 버튼 클릭 함수 호출 리팩토링

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,3 +1,9 @@
+const clickDigitButtons = (digits = []) => {
+  digits.forEach((digit) => {
+    cy.get('.digit').contains(digit).click();
+  });
+};
+
 describe('계산기 앱 테스트', () => {
   beforeEach('페이지 방문', () => {
     cy.visit('../../index.html');
@@ -13,22 +19,19 @@ describe('계산기 앱 테스트', () => {
   it('1개의 숫자 버튼을 클릭하면 display에 숫자가 표시된다', () => {
     // contains()는 해당 문자열이 포함된 여러 HTML 요소 중 하나를 선택함
     // click은 해당 HTML 태그를 클릭처리
-    cy.get('.digit').contains('1').click();
+    clickDigitButtons(['1']);
     cy.get('#total').should('have.text', 1);
   });
 
   // 1개의 클릭보단 큰 단위인 2개의 클릭
   it('2개의 숫자 버튼을 클릭하면 display에 숫자가 표시된다', () => {
-    cy.get('.digit').contains('1').click();
-    cy.get('.digit').contains('2').click();
+    clickDigitButtons(['1', '2']);
     cy.get('#total').should('have.text', 12);
   });
 
   // unknown의 영역 -> 3개까지 잘 작동될까?
   it('2개의 숫자 버튼을 클릭하면 display에 숫자가 표시된다', () => {
-    cy.get('.digit').contains('1').click();
-    cy.get('.digit').contains('2').click();
-    cy.get('.digit').contains('4').click();
+    clickDigitButtons(['1', '2', '4']);
     cy.get('#total').should('have.text', 124);
   });
 });
