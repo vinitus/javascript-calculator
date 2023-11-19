0a8a4eb39a5fbcada933950da46e78ac8cd7d60f
vinitus <tlsdnrng@gmail.com>
2023-11-19 14:25:51 +0900

    연산자에 대한 테스트 코드

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -15,7 +15,7 @@ describe('계산기 앱 테스트', () => {
 
   it('디스플레이에 기본적으로 숫자 0이 표시된다.', () => {
     // querySelector 역할
-    checkDisplayValue(0);
+    checkDisplayValue('0');
   });
 
   // 계산기에서 요구되는 것은 결국 결과값이 표시되어야한다.
@@ -24,18 +24,38 @@ describe('계산기 앱 테스트', () => {
     // contains()는 해당 문자열이 포함된 여러 HTML 요소 중 하나를 선택함
     // click은 해당 HTML 태그를 클릭처리
     clickDigitButtons(['1']);
-    checkDisplayValue(1);
+    checkDisplayValue('1');
   });
 
   // 1개의 클릭보단 큰 단위인 2개의 클릭
   it('2개의 숫자 버튼을 클릭하면 display에 숫자가 표시된다', () => {
     clickDigitButtons(['1', '2']);
-    checkDisplayValue(12);
+    checkDisplayValue('12');
   });
 
   // unknown의 영역 -> 3개까지 잘 작동될까?
-  it('2개의 숫자 버튼을 클릭하면 display에 숫자가 표시된다', () => {
+  it('3개의 숫자 버튼을 클릭하면 display에 숫자가 표시된다', () => {
     clickDigitButtons(['1', '2', '4']);
-    checkDisplayValue(124);
+    checkDisplayValue('124');
+  });
+
+  xit('3개의 숫자 버튼을 누르고 연산자 버튼을 누르면 display에 연산자가 표시된다.', () => {
+    clickDigitButtons(['1', '2', '4']);
+    cy.get('.operation').contain('*').click();
+    checkDisplayValue('124*');
+  });
+
+  xit('연사자 버튼을 여려번 누르면 display에 마지막 연산자만 표시한다.', () => {
+    clickDigitButtons(['1', '2', '4']);
+    cy.get('.operation').contain('*').click();
+    cy.get('.operation').contain('-').click();
+    checkDisplayValue('124-');
+  });
+
+  xit('연사자 다음의 2번째 숫자를 입력하면 display에 2번째 숫자를 누적하여 표시한다.', () => {
+    clickDigitButtons(['1', '2', '4']);
+    cy.get('.operation').contain('*').click();
+    clickDigitButtons(['1', '2', '3']);
+    checkDisplayValue('124*123');
   });
 });
